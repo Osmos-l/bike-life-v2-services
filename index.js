5e0157fe1031d4676f1a898e3584639cdf6a0482
Osmos <emailjeux81@gmail.com>
2022-02-01 17:50:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const run = () => {
+    require('dotenv').config();
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.listen(process.env.PORT)
+    console.log(`Server is running on port ${process.env.PORT}`);
+}
+
+
+try {
+    run();
+} catch (e) {
+    console.error(e);
+}
